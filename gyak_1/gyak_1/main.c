8d8d1eb65b9504fa53148ea660b2e6f5d24f39d0
Fehér Árpád <feher.arpad@kjk.bme.hu>
2024-09-10 16:32:06 +0200

    main.c restored

--- gyak_1/gyak_1/main.c
+++ gyak_1/gyak_1/main.c
@@ -1,27 +1,18 @@
-/*
- * gyak1_2.c
- *
- * Created: 9/5/2024 12:38:17 PM
- * Author : feherarpad
- */ 
-
 #include <avr/io.h>
-#include <avr/interrupt.h>
 
-#define F_CPU 8000000UL 
+#define F_CPU 8000000UL
 #include <avr/delay.h>
-
+#include <avr/interrupt.h>
 #include <inttypes.h>
+
+#define FALSE 0
+#define TRUE 1
+
+uint16_t timer_cnt=0;
+uint8_t task_10ms = FALSE, task_100ms = FALSE, task_500ms = FALSE;
 
 void port_init(void);
 void timer_init(void);
-void external_int_init(void);
-
-#define TRUE 1
-#define FALSE 0
-
-uint8_t task_10ms =FALSE, task_100ms=FALSE, task_500ms=FALSE;
-uint16_t timer_cnt=0;
 
 void port_init(void)
 {
@@ -30,49 +21,40 @@ void port_init(void)
 	
 	DDRF = 0x0f;
 	PORTF = 0x0f;
-	
-	DDRD = (0<<PD0);
-	PORTD = (1<<PD0);
 }
+
 
 void timer_init(void)
 {
-	TCCR0A = (0<<WGM00) | (1<<WGM01) | (1<<CS02) | (0<<CS01) | (1<<CS00);
+	TCCR0A = (1<<WGM01) | (0<<WGM00) | (1<<CS2) | (0<<CS1) | (1<<CS0);
 	OCR0A = 77;
 	TIMSK0 = (1<<OCIE0A);
 }
 
-void external_int_init(void)
-{
-	EICRA = (1<<ISC01) | (0<<ISC00);
-	EIMSK = (1<<INT0);
-}
 
 int main(void)
 {
 	port_init();
 	timer_init();
-	external_int_init();
 	sei();
+	
     /* Replace with your application code */
-    while (1)
+    while (1) 
     {
-		if(task_10ms == TRUE)
+		if(task_10ms)
 		{
-			PORTF ^= 0x01;
-			task_10ms = FALSE;
+			PORTF ^=(1<<PF0);
+			task_10ms=FALSE;
 		}
-		
-		if(task_100ms == TRUE)
+		if(task_100ms)
 		{
-			PORTF ^= 0X02;
-			task_100ms = FALSE;
+			PORTF ^=(1<<PF1);
+			task_100ms=FALSE;
 		}
-		
-		if(task_500ms == TRUE)
+		if(task_500ms)
 		{
-			PORTF ^= 0x04;
-			task_500ms = FALSE;
+			PORTF ^=(1<<PF2);
+			task_500ms=FALSE;
 		}
     }
 }
@@ -80,15 +62,7 @@ int main(void)
 ISR(TIMER0_COMP_vect)
 {
 	timer_cnt++;
-	if((timer_cnt % 1) == 0) task_10ms = TRUE;
-	if((timer_cnt % 10) == 0) task_100ms =TRUE;
-	if((timer_cnt % 50) == 0) task_500ms = TRUE;
-	
-	
+	if(timer_cnt % 1 == 0) task_10ms = TRUE;
+	if(timer_cnt % 10 == 0) task_100ms = TRUE; 
+	if(timer_cnt % 50 == 0) task_500ms =TRUE;
 }
-
-ISR(INT0_vect)
-{
-	PORTA = PORTA ^ 0x01;
-}
-
