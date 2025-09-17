d138e5fcca28522f7295d0f3a1cbd2275e5f3eee
Fehér Árpád <feher.arpad@kjk.bme.hu>
2025-09-17 15:50:15 +0200

    09.17

--- gyak/gyak/main.c
+++ gyak/gyak/main.c
@@ -18,6 +18,8 @@
 #define TRUE 1
 #define FALSE 0
 
+#define PD0_ENA_DELAY 80 //ms
+
 /******************************************************************************
 * Constants
 ******************************************************************************/
@@ -28,6 +30,9 @@
 ******************************************************************************/
 uint16_t timer_cnt=0;
 uint8_t timer_task_10ms=0, timer_task_100ms=0, timer_task_500ms=0, timer_task_1s=0;
+uint8_t PD0_re_enable_cnt=0;
+uint8_t PB0_pushed=0;
+uint16_t ad_value = 0;
 
 /******************************************************************************
 * External Variables
@@ -39,7 +44,8 @@ uint8_t timer_task_10ms=0, timer_task_100ms=0, timer_task_500ms=0, timer_task_1s
 ******************************************************************************/
 void timer_init(void);
 void port_init(void);
-
+void external_interrupt_init(void);
+void ad_init(void);
 
 
 /******************************************************************************
@@ -48,7 +54,13 @@ void port_init(void);
 void port_init(void)
 {
 	DDRA = 0xff;
-	DDRF = (1<<PF0) | (1<<PF1) | (1<<PF2) | (1<<PF3);
+	DDRF = (0<<PF0) | (1<<PF1) | (1<<PF2) | (1<<PF3);
+	
+	DDRD = (0<<PD0);
+	PORTD = (1<<PD0);
+	
+	DDRB = (0<<PB0);
+	PORTB = (1<<PB0);
 }
 
 void timer_init(void)
@@ -56,6 +68,20 @@ void timer_init(void)
 	TCCR0A = (0<<WGM00) | (1<<WGM01) | (1<<CS02) | (0<<CS01) | (1<<CS00);
 	OCR0A = 77;
 	TIMSK0 = (1<<OCIE0A);
+}
+
+void external_interrupt_init(void)
+{
+	EICRA = (0<<ISC00) | (1<<ISC01);
+	EIMSK = (1<<INT0);
+}
+
+void ad_init(void)
+{
+	ADMUX = 0;
+	ADCSRA = (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2) | (1<<ADEN) | (1<<ADIE) | (1<<ADSC);
+	ADCSRB = 0;
+	
 }
 
 /******************************************************************************
@@ -69,17 +95,31 @@ int main(void)
 {
 	port_init();
 	timer_init();
+	external_interrupt_init();
+	ad_init();
 	sei();
 	/* Replace with your application code */
 	while(1)
 	{
 		if(timer_task_10ms)
 		{
-			PORTF ^= (1<<PF0);
+			if((PINB & (1<<PB0)) == 0 && PB0_pushed == 0)
+			{
+				PORTA ^=0x01;
+				PB0_pushed=1;
+			}
+			if((PINB & (1<<PB0)) == (1<<PB0) && PB0_pushed == 1) PB0_pushed = 0;
+			
+			
+			if(PD0_re_enable_cnt<PD0_ENA_DELAY) PD0_re_enable_cnt += 10;
+			//PORTF ^= (1<<PF0);
 			timer_task_10ms=FALSE;
 		}
 		if(timer_task_100ms)
 		{
+			ADCSRA |= (1<<ADSC);
+			PORTA = ad_value>>2;
+			
 			PORTF ^= (1<<PF1);
 			timer_task_100ms=FALSE;
 		}
@@ -109,7 +149,16 @@ ISR(TIMER0_COMP_vect)
 	if((timer_cnt % 100) == 0) timer_task_1s = TRUE;
 }
 
+ISR(INT0_vect)
+{
+	if(PD0_re_enable_cnt == PD0_ENA_DELAY)
+	{
+		PORTA ^=0xff;
+		PD0_re_enable_cnt=0;
+	}
+}
 
-
-
-
+ISR(ADC_vect)
+{
+	ad_value = ADC;
+}
